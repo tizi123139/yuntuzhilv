3cffaf75eef98fd2d33296d14a8c15ffce22b152
tizi <1357443586@qq.com>
2026-07-21 15:39:19 +0800

    项目框架

---
+++ yuntuzhilv/back-travel/src/main/java/com/travel/backtravel/BackTravelApplication.java
@@ -0,0 +1,13 @@
+package com.travel.backtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackTravelApplication.class, args);
+    }
+
+}
---
+++ yuntuzhilv/back-travel/src/test/java/com/travel/backtravel/BackTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.backtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
